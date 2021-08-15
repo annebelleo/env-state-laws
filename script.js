9f21fc5fe6cf128e1a41873e0e5a8e3969874f98
Annebelle Olminkhof <annebelleolminkhof@gmail.com>
2021-08-15 00:31:11 -0700

    qoom sync at Sun Aug 15 2021 07:31:10 GMT+0000 (Coordinated Universal Time)

--- script.js
+++ script.js
@@ -6,21 +6,18 @@ var laws;
 
 var path = d3.geoPath();
 
-// var x = d3.scaleLinear(),
-//     .domain([1, 10]),
-//     .rangeRound([600, 860]);
-
-// var color = d3.scaleThreshold(),
-//     .domain(d3.range(0, 10)),
-//     .range(d3.schemeBlues[9]);
+var states;
+var borders;
+var lawsToFill = [];
 
 const state_id = 
-{1: "Alabama", 2: "Alaska", 4: "Arizona", 5: "Arkansas", 6: "California", 8: "Colorado", 9: "Connecticut", 10: "Delaware", 12: "Florida", 13: "Georgia", 15: "Hawaii", 16: "Idaho", 17: "Illinois", 18: "Indiana", 19: "Iowa", 20: "Kansas", 21: "Kentucky", 22: "Louisiana", 23: "Maine", 24: "Maryland", 25: "Massachusetts", 26: "Michigan", 27: "Minnesota", 28: "Mississippi", 29: "Missouri", 30: "Montana", 31: "Nebraska", 32: "Nevada", 33: "New Hampshire", 34: "New Jersey", 35: "New Mexico", 36: "New York", 37: "North Carolina", 38: "North Dakota", 39: "Ohio", 40: "Oklahoma", 41: "Oregon", 42: "Pennsylvania", 44: "Rhode Island", 45: "South Carolina", 46: "South Dakota", 47: "Tennessee", 48: "Texas", 49: "Utah", 50: "Vermont", 51: "Virginia", 53: "Washington", 54: "West Virginia", 55: "Wisconsin", 56:	"Wyoming
+{"01": "Alabama", "02": "Alaska", "04": "Arizona", "05": "Arkansas", "06": "California", "08": "Colorado", "09": "Connecticut",  "10": "Delaware", "12": "Florida", "13": "Georgia", "15": "Hawaii", "16": "Idaho", "17": "Illinois", "18": "Indiana", "19": "Iowa", "20": "Kansas", "21": "Kentucky", "22": "Louisiana", "23": "Maine", "24": "Maryland", "25": "Massachusetts", "26": "Michigan", "27": "Minnesota", "28": "Mississippi", "29": "Missouri", "30": "Montana", "31": "Nebraska", "32": "Nevada", "33": "New Hampshire", "34": "New Jersey", "35": "New Mexico", "36": "New York", "37": "North Carolina", "38": "North Dakota", "39": "Ohio", "40": "Oklahoma", "41": "Oregon", "42": "Pennsylvania", "44": "Rhode Island", "45": "South Carolina", "46": "South Dakota", "47": "Tennessee", "48": "Texas", "49": "Utah", "50": "Vermont", "51": "Virginia", "53": "Washington", "54": "West Virginia", "55": "Wisconsin", "56": "Wyoming"}
 
 var promises = [
 d3.json("https://d3js.org/us-10m.v1.json"),
 d3.csv("envstate.csv").then(function(d) {
 	laws = d;
+	console.log(laws)
 })
 ]
 
@@ -28,18 +25,48 @@ console.log("before promises")
 Promise.all(promises).then(ready)
 	
 function ready([us]) {
-	
-  svg.append("g")
+
+  //  d3.selectAll("input")
+		
+		
+  states = svg.append("g")
       .attr("class", "states")
     .selectAll("path")
     .data(topojson.feature(us, us.objects.states).features)
     .enter().append("path")
-      .attr("d", path)
-      .attr("fill", function(d) {
-      	console.log("d", d)
-      })
-
-  svg.append("path")
+    .attr("d", path)
+    .attr("fill", "gray")
+    .on("click", function(d) {
+    	d3.selectAll("p").remove()
+    	d3.selectAll("h3").remove()
+    	var currentState = state_id[d.id]
+    	currentState=currentState.replace(/\s/g, "")
+    	console.log(currentState)
+    	d3.selectAll('.' + currentState).append('h3').text("Environmental Laws in " + currentState + ":")
+    	currentState = state_id[d.id]
+    	console.log(currentState)
+    	for (var i=0; i<laws.length; i++) {
+    		if (laws[i].state == currentState) {
+    			currentState=currentState.replace(/\s/g, "")
+    			var appendLink = d3.selectAll('.' + currentState).append('a')
+    			.attr("xlink:href", laws[i].sources)
+    			.append('p').text(laws[i].title)
+    		}
+    	}
+		})
+    
+  borders = svg.append("path")
       .attr("class", "state-borders")
       .attr("d", path(topojson.mesh(us, us.objects.states, function(a, b) { return a !== b; })));
+      
+  
+    // plan for tomorrow:
+    // event listener on all the law buttons (done)
+    // for a change on a button, fetch label (done)
+    // parse through laws to find objects with title that match label (done)
+    // fetch states from objects (done)
+    // match to state ids
+    // pull state ids from svg and fill ones that match
+    
+
 }
